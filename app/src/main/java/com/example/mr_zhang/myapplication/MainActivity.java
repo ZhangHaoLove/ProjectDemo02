e34427f3a4bcac8d3dc2b64e3fc42d5a5e171955
ZhangHaoLove <1957247605@qq.com>
2017-09-12 08:43:35 +0800

    第一次

---
+++ app/src/main/java/com/example/mr_zhang/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.mr_zhang.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void set(){
+
+    }
+    public void set1(){
+
+    }
+}
